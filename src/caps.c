828c079951c1510095167144791c578a7b792422
cvernon20 <cjacevernon@gmail.com>
2025-03-22 13:35:27 -0700

    updated caps

--- src/caps.c
+++ src/caps.c
@@ -11,8 +11,8 @@ u32 GetCurrentLevelCap(void)
     {
         {FLAG_BADGE01_GET, 16},
         {FLAG_BADGE02_GET, 23},
-        {FLAG_BADGE03_GET, 23},
-        {FLAG_BADGE04_GET, 29},
+        {FLAG_BADGE03_GET, 34},
+        {FLAG_BADGE04_GET, 44},
         {FLAG_BADGE05_GET, 31},
         {FLAG_BADGE06_GET, 33},
         {FLAG_BADGE07_GET, 42},
