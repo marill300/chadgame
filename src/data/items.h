5fb4112747e53d2f42f6f85dbdb40d99cf706e4f
marill300 <gigabyte104@gmail.com>
2025-04-29 19:45:47 -0400

    level cap candy code (credit AlexOn1ine)

--- src/data/items.h
+++ src/data/items.h
@@ -2025,6 +2025,25 @@ const struct Item gItemsInfo[] =
         .iconPalette = gItemIconPalette_ExpCandies,
     },
 
+    [ITEM_SUPER_CANDY] =
+    {
+        .name = _("Super Candy"),
+        .pluralName = _("Super Candies"),
+        .price = 0,
+        .holdEffectParam = LEVEL_CAP,
+        .description = COMPOUND_STRING(
+            "Raises level to\n"
+            "the level cap."),
+        .importance = 1,
+        .pocket = POCKET_KEY_ITEMS,
+        .type = ITEM_USE_PARTY_MENU,
+        .fieldUseFunc = ItemUseOutOfBattle_RareCandy,
+        .effect = gItemEffect_RareCandy,
+        .flingPower = 30,
+        .iconPic = gItemIcon_ExpCandyXL,
+        .iconPalette = gItemIconPalette_ExpCandies,
+    },
+
     [ITEM_DYNAMAX_CANDY] =
     {
         .name = _("Dynamax Candy"),
