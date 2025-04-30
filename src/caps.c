4a9939fc95db22f02605b4650365ffd46616ad9f
cvernon20 <cjacevernon@gmail.com>
2025-04-29 18:07:53 -0700

    updated level cap at 4th gym

--- src/caps.c
+++ src/caps.c
@@ -12,7 +12,7 @@ u32 GetCurrentLevelCap(void)
         {FLAG_BADGE01_GET, 16},
         {FLAG_BADGE02_GET, 23},
         {FLAG_BADGE03_GET, 35},
-        {FLAG_BADGE04_GET, 44},
+        {FLAG_BADGE04_GET, 48},
         {FLAG_BADGE05_GET, 31},
         {FLAG_BADGE06_GET, 33},
         {FLAG_BADGE07_GET, 42},
