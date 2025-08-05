1961c5d4cb14a0cb1fada5b0a7536e56f645050b
marill300 <gigabyte104@gmail.com>
2025-08-04 23:29:32 -0400

    debug and testing additions/qol

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1314,7 +1314,7 @@
 #define FLAG_UNUSED_0x4EC                                           0x4EC // Unused Flag
 #define FLAG_UNUSED_0x4ED                                           0x4ED // Unused Flag
 #define FLAG_UNUSED_0x4EE                                           0x4EE // Unused Flag
-#define FLAG_UNUSED_0x4EF                                           0x4EF // Unused Flag
+#define FLAG_UNUSED_0x4EF                                           0x4EF // collisions debug
 
 #define FLAG_DEFEATED_RUSTBORO_GYM                                  0x4F0
 #define FLAG_DEFEATED_DEWFORD_GYM                                   0x4F1
