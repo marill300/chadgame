7849e52a8598de094d29d456d402a74ce2f4064f
cvernon20 <cjacevernon@gmail.com>
2025-08-17 15:32:47 -0700

    fixed caps

--- src/caps.c
+++ src/caps.c
@@ -10,9 +10,9 @@ u32 GetCurrentLevelCap(void)
     static const u32 sLevelCapFlagMap[][2] =
     {
         {FLAG_BADGE01_GET, 15},
-        {FLAG_BADGE02_GET, 29},
-        {FLAG_BADGE03_GET, 39},
-        {FLAG_BADGE04_GET, 46},
+        {FLAG_BADGE02_GET, 46},
+        {FLAG_BADGE03_GET, 29},
+        {FLAG_BADGE04_GET, 39},
         {FLAG_BADGE05_GET, 50},
         {FLAG_BADGE06_GET, 60},
         {FLAG_BADGE07_GET, 70},
